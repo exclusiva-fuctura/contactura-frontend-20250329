e9cf72da87df36909704a35afd253106bf1f83c1
Gustavo Marinho <ghmarinho@gmail.com>
2025-04-25 08:59:40 -0300

    aula 12-04-2025

---
+++ src/app/lancamentos/lancamentos.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LancamentosRoutingModule } from './lancamentos-routing.module';
+import { DespesasComponent } from './despesas/despesas.component';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LancamentosRoutingModule
+  ]
+})
+export class LancamentosModule { }
